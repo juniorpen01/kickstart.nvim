57bf41a8335e3e1e5a8f935bfc1e6ecf310055de
juniorpen01 <01juniorpen@gmail.com>
2025-06-02 14:07:01 +0800

    Switch back to Treesitter autoinstalling

--- init.lua
+++ init.lua
@@ -956,26 +956,9 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'diff',
-        'html',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc',
-        'svelte',
-        'json',
-        'ssh_config',
-        'python',
-        'git_config',
-      },
-      -- -- Autoinstall languages that are not installed
-      auto_install = false,
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
       highlight = {
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
