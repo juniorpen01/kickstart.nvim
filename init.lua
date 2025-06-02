6b4c8994ff486318995da1731df7d0cd0d08d544
juniorpen01 <01juniorpen@gmail.com>
2025-06-02 11:43:01 +0800

    Add file associations for `gitconfig` and `gitconfig.local`; ensure Treesitter parsers are installed for Python, SSH, and gitconfig

--- init.lua
+++ init.lua
@@ -956,7 +956,24 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'svelte', 'json' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'svelte',
+        'json',
+        'ssh_config',
+        'python',
+        'git_config',
+      },
       -- -- Autoinstall languages that are not installed
       auto_install = false,
       highlight = {
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.filetype.add {
+  filename = {
+    gitconfig = 'gitconfig',
+    ['gitconfig.local'] = 'gitconfig',
+  },
+}
+
 return {
   {
     'stevearc/oil.nvim',
@@ -12,8 +20,18 @@ return {
   },
   {
     'ellisonleao/gruvbox.nvim',
+    opts = {},
     config = function()
       vim.cmd.colorscheme 'gruvbox'
     end,
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
   },
 }
